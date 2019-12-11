23465d96ac057d59dfcb955c7b099ff6ee6f295d
Subroto <subrotoph@gmail.com>
2019-12-11 17:43:22 +0900

    Implement BoardNode in GameScene

--- Gomoku/Object/GameScene.swift
+++ Gomoku/Object/GameScene.swift
@@ -5,8 +5,22 @@ import UIKit
 import SpriteKit
 
 class GameScene: SKScene {
+    var boardNode: BoardNode = BoardNode()
+    var halfSize: CGSize = CGSize()
+    var boardSize: CGFloat = 0
     
     func settupBoard() {
+        scaleMode = .resizeFill
+        anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        
+        halfSize = CGSize(width: size.width/2, height: size.height/2)
+        boardSize = max(size.width, size.height)
+        
+        boardNode.removeFromParent()
+        boardNode = BoardNode()
+        boardNode.prepare(size: boardSize)
+        boardNode.position = CGPoint(x: -boardNode.size/2, y: -boardNode.size/2)
+        addChild(boardNode)
     }
     
 }
