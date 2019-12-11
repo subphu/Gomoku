4bfb1d9624cd9135aff4f4a4a4c5ad520ffbaaab
Subroto <subrotoph@gmail.com>
2019-12-11 19:00:08 +0900

    Implement gesture in watch

--- Gomoku WatchKit Extension/GameInterfaceController.swift
+++ Gomoku WatchKit Extension/GameInterfaceController.swift
@@ -8,15 +8,45 @@ import SpriteKit
 class GameInterfaceController: WKInterfaceController {
     
     @IBOutlet weak var spriteKit: WKInterfaceSKScene!
-
+    
+    var scene: GameScene! = nil
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         crownSequencer.delegate = self
         crownSequencer.focus()
-        let scene = GameScene(size: CGSize(width: 450, height: 450))
+        
+        var size = contentFrame.size
+        size.height -= contentFrame.origin.y
+
+        scene = GameScene(size: size)
         scene.settupBoard()
         spriteKit.presentScene(scene)
+    }
+    
+    @IBAction func tap(_ sender: WKTapGestureRecognizer) {
+        let location = sender.locationInObject()
+        var normalized = CGPoint()
+        normalized.x = location.x - scene.halfSize.width
+        normalized.y = scene.halfSize.height - location.y
+        scene.tap(in: normalized)
+    }
+    
+    @IBAction func press(_ sender: Any) {
+    }
+    
+    var prevTranslation: CGPoint = CGPoint(x: 0, y: 0)
+    @IBAction func pan(_ sender: WKPanGestureRecognizer) {
+        guard sender.state != .ended else { return }
+        let translation = sender.translationInObject()
+        let normalized = CGPoint(x: translation.x, y: -translation.y)
+        var delta = normalized
+        if sender.state == .changed {
+            delta.x = normalized.x - prevTranslation.x
+            delta.y = normalized.y - prevTranslation.y
+        }
+        scene.move(translation: delta)
+        prevTranslation = normalized
     }
     
     override func willActivate() {
@@ -27,5 +57,6 @@ class GameInterfaceController: WKInterfaceController {
 
 extension GameInterfaceController: WKCrownDelegate {
     func crownDidRotate(_ crownSequencer: WKCrownSequencer?, rotationalDelta: Double) {
+        scene.zoom(by: CGFloat(1 + rotationalDelta))
     }
 }
